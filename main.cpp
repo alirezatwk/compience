6e45cbf88bd4b4da6327f7466c6285497e343b0d
ali.amelie <ali.amelie@cafebazaar.ir>
2019-03-06 09:15:45 +0330

    Add support for negative numbers

--- main.cpp
+++ main.cpp
@@ -23,33 +23,29 @@ int main(){
 
 
 	int result = 0;
-	bool wait_for_number = true, error = false;
+	bool wait_for_number = true;
 	for ( std::vector<std::string>::iterator it = elements.begin(); it != elements.end(); ++it ) {
+		if ((*it) == "+"){
+			if (wait_for_number) {
+				std::cout << "ERROR" << std::endl;
+				return -1;
+			} else {
+				wait_for_number = true;
+				continue;
+			}
+		}
+
 		bool is_number = true;
-		for ( std::string::iterator cit = it->begin(); cit != it->end(); ++cit)
+		for ( std::string::iterator cit = it->begin() + ((*it)[0] == '-' ? 1 : 0); cit != it->end(); ++cit)
 			if (not isdigit(*cit))
 				is_number = false;
 
-		if (is_number) {
-			if (wait_for_number) {
-				result += std::atoi(it->c_str());
-				wait_for_number = false;
-			} else {
-				std::cout << "ERROR" << std::endl;
-				return -1;
-			}
+		if (is_number && wait_for_number) {
+			result += std::atoi(it->c_str());
+			wait_for_number = false;
 		} else {
-			if (wait_for_number) {
-				std::cout << "ERROR" << std::endl;
-				return -1;
-			} else {
-				if ((*it) == "+"){
-					wait_for_number = true;
-				} else {
-					std::cout << "ERROR" << std::endl;
-					return -1;
-				}
-			}
+			std::cout << "ERROR" << std::endl;
+			return -1;
 		}
 	}
 
